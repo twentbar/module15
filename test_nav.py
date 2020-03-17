0fa6ce6e6ee9a83a11ad94273fe6d0a64b4aeafc
alex-pancho <panix.p@yandex.ru>
2020-03-17 17:29:30 +0200

    update test covered

--- test_nav.py
+++ test_nav.py
@@ -10,8 +10,10 @@ class NavigationTest(BaseCase):
                      "Сlothing":['//a[@data-class="babyclothes"]', self.base_url+"/clothing/s/619"],
                      "Footwear":['//a[@data-class="babyshoes"]',self.base_url+"/footwear/s/620"]
             }
+
+    
     # проверяем пункты меню
-    def test_menu(self):
+    def test_01_menu(self):
         self.get(self.base_url)
         for item in self.menu_dict:
             print(f"Меню {item}")
@@ -21,6 +23,68 @@ class NavigationTest(BaseCase):
             # проверка соответствия полученного и ожидаемого урл
             self.assert_equal(curr_url, self.menu_dict[item][1])
 
-    def test_sub_menu(self):
+    def test_02_sub_menu(self):
         self.get(self.base_url)
-        self.hover_and_click('a[data-class="babyshoes"]', '//div[@class="babyshoes mid-navigation-container"]/div[@class="content c-16"]/div[@class="c-8"]/ul[2]/li[1]/a')
+        sub = '//*[@id="mid-navigation"]/div[9]/div/div[1]/ul[2]/li[1]/a' #'//a[@href="/boots-and-winter-shoes/s/673"]'
+        self.hover_and_click('//a[@data-class="babyshoes"]', sub)
+    
+    def test_03_find_items(self):
+
+        self.get(self.base_url)
+        # send_keys - это печать текста в поле (отправка клавиш)
+        self.send_keys('//*[@id="search-form"]/input', "Sirona S i-Size Car Seat")
+        # клик на кнопке поиска
+        self.click('//*[@id="search-form"]/button')
+        # получаем  все элементы по заданному х-пазу и считаем длинну списка 
+        count = len(self.find_elements("//article"))
+        # сверяем длину и ожидаемый результат
+        self.assert_equal(count,5)
+        # пример, как можно задать паузу
+        self.sleep(2)
+  
+    def test_04_add_to_basket(self):
+        # Идем на страницу товаров
+        self.get(self.base_url+'/dolce-gabbana/s/1495')
+        # сохраняем название первого товара страницы
+        item_name = self.get_text('//article[1]/a/div[2]/p[2]')
+        # эмулируем наведение мышкой и клик на кнопке "Add to cart"
+        self.hover_and_click('//article[1]','//article[1]/a/div[1]/div/div[1]/p')
+        # выбираем размер в выпадающем меню
+        self.click('//*[@id="id-slct"]/div')
+        self.click('//*[@id="id-slct"]/ul/li[2]')
+        # эмулируем наведение мышкой и клик на кнопке "Add to cart"
+        self.click('//article[1]/div[1]/form/div/button/span[1]')
+        # чуть ждем отработки скрипта
+        self.sleep(0.1)
+        # переходим к оплате
+        self.click('//article[1]/div[1]/form/div/a')
+        # сохраняем название товара в корзине
+        basket_name = self.get_text('//a[@class="product-name c-6"]/p[2]')
+        # название выбраного товара и товара в корзине должно совпасть
+        self.assert_equal(item_name,basket_name)
+        # удаляем товар
+        self.click('//a[@class="remove"]')
+        #self.sleep(0.1)
+        # Получаем сообщение, что корзина пуста
+        empty_text = self.get_text('//div[@class="simple-page"]/h1')
+        # проверяем его
+        self.assert_equal(empty_text, "Your cart is empty")
+
+    def test_05_change_region(self):
+        # 
+        self.get(self.base_url+'/dolce-gabbana/s/1495')
+        # получаем английский текст
+        eng_text = self.get_text('//article[1]/a/div[2]/p[2]')
+        # переход в раздел языковых параметров и их смена
+        self.click('//*[@id="top-navigation-links"]/ul/li[1]/a')
+        self.click('//a[@title="Russian"]')
+        self.sleep(0.1)
+        self.click('//a[@title="USA"]')
+        # переход в раздел товаров на русском
+        self.get('https://ru.babyshop.com/dolce-gabbana/s/1495')
+        # получаем русский текст
+        ru_text = self.get_text('//article[1]/a/div[2]/p[2]')
+        # английский текст не должен совпадать с русским
+        self.assert_not_equal(eng_text, ru_text)
+
+
